4fb6365f9fb149b8b4f8c5aef0839bb41ffab8a2
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2021-04-16 01:20:14 +0200

    alternate formats for reading

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,6 @@ import Options.Applicative
 import Text.Parsec
 import Data.Maybe
 
-import qualified Data.Aeson as AE
 import qualified Data.Yaml as Y
 import qualified Data.List as L
 import qualified Data.Text as T
@@ -24,10 +23,14 @@ data ParseException = ParseException String
 
 instance Exception ParseException
 
-flattenMain :: IO ()
-flattenMain = do
+flattenMain :: Options -> IO ()
+flattenMain opts = do
   s <- getContents
-  docs <- readYAMLRecords s
+  docs <- case (optFormat opts) of
+    "json" -> readRecordJSON s
+    "jsonlines" -> readRecordsJSONLines s
+    "yaml" -> readRecordsYAML s
+    k -> throwIO $ ParseException $ "bad format: " ++ k
   putStr $ unlines $ map formatFlattened $ flatten docs
 
 partitionEithersTerminating :: [Either c a] -> (Maybe c, [a])
@@ -43,8 +46,8 @@ showResultOrDie result = case result of
          putStrLn "---"
          BL.putStr $ Y.encode x
 
-unflattenMain :: IO ()
-unflattenMain = do
+unflattenMain :: Options -> IO ()
+unflattenMain opts = do
    s <- getContents
    (maybeErr, flats) <- return $ partitionEithersTerminating $ parseManyFlattened (lines s)
    sequence $ map showResultOrDie $ unflatten flats
@@ -55,19 +58,24 @@ unflattenMain = do
 
 data Options = Options
   { optReverse :: Bool
+  , optFormat :: String
   }
 
 argparser = info opts (progDesc "Tool to convert YAML-ish data to TSV and back." <> fullDesc)
   where
-    opts = Options <$>
-      switch (long "reverse" <>
-              short 'r' <>
-              help "Un-flatten instead of flatten.") <*
-      abortOption ShowHelpText (long "help" <>
-                                help "Display this message.")
+    opts = Options
+      <$> switch (long "reverse"
+                  <> short 'r'
+                  <> help "Un-flatten instead of flatten")
+      <*> strOption (long "format" <>
+                     short 'f' <>
+                     value "yaml" <>
+                     help "Non-flat format (json, yaml)")
+      <* abortOption ShowHelpText (long "help" <>
+                                   help "Display this message")
 main :: IO ()
 main = do
   args <- execParser argparser
   case (optReverse args) of
-    True -> unflattenMain
-    False -> flattenMain
+    True -> unflattenMain args
+    False -> flattenMain args
--- src/Flatter/Read.hs
+++ src/Flatter/Read.hs
@@ -1,12 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 module Flatter.Read
-    ( readYAMLRecords
+    ( readRecordsYAML
+    , readRecordJSON
+    , readRecordsJSONLines
     ) where
 
-import qualified Data.ByteString.UTF8 as UTF8
-import qualified Data.Yaml as Y
+import Data.Typeable
+import Control.Exception
+import Control.Monad (liftM, join)
+import qualified Data.Text as T
+import Data.Maybe (catMaybes)
 
-readYAMLRecords :: String -> IO [Y.Value]
-readYAMLRecords s = do
+import qualified Data.ByteString.UTF8 as UTF8
+import qualified Data.ByteString.Lazy.UTF8 as LUTF8
+import qualified Data.Yaml as Y
+import qualified Data.Aeson as AE
+
+data ParseException = ParseException String
+  deriving (Show, Typeable)
+
+instance Exception ParseException
+
+readRecordsYAML :: String -> IO [AE.Value]
+readRecordsYAML s = do
   rv <- Y.decodeAllThrow $ UTF8.fromString s
   return rv
 
+readRecordJSON :: String -> IO [AE.Value]
+readRecordJSON s = do
+  case (AE.decode $ LUTF8.fromString s) of
+    Nothing -> throwIO $ ParseException "bad JSON data"
+    Just rv -> return [rv]
+
+readRecordsJSONLines :: String -> IO [AE.Value]
+readRecordsJSONLines s = do
+  h
+    where
+      g :: IO [Maybe AE.Value]
+      g = sequence $ map (f . T.unpack . T.strip . T.pack) $ lines s
+
+      h :: IO [AE.Value]
+      h = liftM catMaybes g
+
+      f :: String -> IO (Maybe AE.Value)
+      f x = if (null x)
+            then return Nothing
+            else case (AE.decode $ LUTF8.fromString x) of
+              Nothing -> throwIO $ ParseException $ "bad JSON data: " ++ x
+              Just rv -> return $ Just rv
